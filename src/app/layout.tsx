4a4e8917dc1ec6a603f5be3cce25a1539f348834
Yogesh Jagtap <Jagtapyogesh41@gmail.com>
2025-12-29 09:29:16 +0000

    when i am importing this in it there is not "Time Weaver" name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <head>
+        <title>Time Weaver</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
